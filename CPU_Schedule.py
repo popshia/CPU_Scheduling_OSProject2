888a62897296ee209a2473e66a31612f54249fb7
popshia <B123292179@gmail.com>
2020-06-07 12:24:11 +0800

    PSJF done, RR still unfixed :)

--- CPU_Schedule.py
+++ CPU_Schedule.py
@@ -11,7 +11,7 @@
 #   • 由剩餘CPU Burst最小的Process先處理。
 #   • 若剩餘的CPU Burst相同，讓沒有用過CPU 的先使用，無法分別時則依arrival time小的先處理。
 #   • 若剩餘CPU Burst相同且arrival time相同，則依ProcessID由小至大依序處理。
-# 4: NSJF ( Non-preemptive Shortest Job First )
+# 4: NSJF ( Non-preemptive Shortest Job First ) 
 #   • 由CPU Burst最小的Process先處理
 #   • 若CPU Burst最少的Process不只一個，則依 arrival time小的先處理
 #   • 若CPU Burst及arrival time相同，則依 ProcessID由小至大依序處理。
@@ -42,7 +42,7 @@ class Process():
         self.Turnaround_Time = 0
         self.Has_Use_CPU = False
 
-class FCFS():
+class FCFS(): # done
     def __init__(self, processList, timeSlice):
         self.Process_List = processList
         self.Time_Slice = timeSlice
@@ -87,7 +87,7 @@ class FCFS():
         self.Print() # print the gantt chart
         self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
 
-class RR():
+class RR(): # buggy
     def __init__(self, processList, timeSlice):
         self.Process_List = processList
         self.Time_Slice = timeSlice
@@ -153,11 +153,63 @@ class RR():
         self.Print() # print the gantt chart
         self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
 
-class PSJF():
-    def __init__(self):
-        super().__init__()
+class PSJF(): # done
+    def __init__(self, processList, timeSlice):
+        self.Process_List = processList
+        self.Time_Slice = timeSlice
+        self.Gantt_Chart = "-"
+        self.Running_Process = None
+        self.Waiting_Queue = queue.Queue()
+        self.Done_List = []
+        self.Process_Quantity = len(processList)
+        self.Current_Time = 1
 
-class NSJF():
+    def CheckProcess(self):
+        for process in self.Process_List: # search the process list
+            if process.Arrival_Time <= self.Current_Time: # if the process have arrived
+                self.Waiting_Queue.put(self.Process_List.pop(self.Process_List.index(process))) # put the process in the waiting queue
+
+    def RunProcess(self):
+        if not self.Running_Process: # if there's no current running process 
+            if self.Waiting_Queue.qsize() > 0: # if there's next process in queue
+                self.Running_Process = self.Waiting_Queue.get() # get the first process in waiting queue
+            else: # if there's no more process in queue
+                return # return
+        self.Running_Process.CPU_Burst_Minus -= 1 # run the process and minus the cpu bust time
+        self.Running_Process.Has_Use_CPU = True
+        self.Gantt_Chart += hex(self.Running_Process.ID)[2:].upper() # add the process ID in hexidecimal into the gantt chart string
+        if self.Running_Process.CPU_Burst_Minus == 0: # if the process has complete
+            self.Running_Process.Complete_Time = self.Current_Time # assign the complete time
+            self.Running_Process.Turnaround_Time = self.Running_Process.Complete_Time - self.Running_Process.Arrival_Time # calculate the turnaround time
+            self.Running_Process.Waiting_Time = self.Running_Process.Turnaround_Time - self.Running_Process.CPU_Burst # calculate the waiting time
+            self.Done_List.append(self.Running_Process) # append the complete process into done list
+            self.Running_Process = None # set running process to none
+        else:
+            self.Waiting_Queue.put(self.Running_Process) # put the current running process back to the waiting queue to reorder
+            self.Running_Process = None # set current running process to none
+
+    def ReorderWaitingQueue(self):
+        temp = [] # create a temp list
+        for _ in range(self.Waiting_Queue.qsize()): temp.append(self.Waiting_Queue.get()) # copy the waiting queue to the temp list
+        temp.sort(key=lambda process: (process.CPU_Burst_Minus, process.Has_Use_CPU, process.Arrival_Time, process.ID)) # sort the process first by cpu burst second has use CPU third by arrival time forth by PID
+        for process in temp: self.Waiting_Queue.put(process) # put the temp list back to the waiting queue
+
+    def Print(self):
+        print("==    PSJF==") # print label
+        print(self.Gantt_Chart) # print gantt chart
+
+    def Start(self):
+        self.Process_List.sort(key=lambda process: (process.CPU_Burst, process.Arrival_Time, process.ID)) # sort the process first by cpu burst second by arrival time third by PID
+        for process in self.Process_List: process.CPU_Burst_Minus = process.CPU_Burst # add cpu burst minus
+        while len(self.Done_List) < self.Process_Quantity: # while length of done list is less than the length of process list
+            self.CheckProcess() # check the upcoming process
+            self.ReorderWaitingQueue() # reorder the waiting queue
+            self.Current_Time += 1 # current time + 1
+            self.RunProcess() # run the current process or dispatch from waiting queue
+        self.Print() # print the gantt chart
+        self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
+
+class NSJF(): # done
     def __init__(self, processList, timeSlice):
         self.Process_List = processList
         self.Time_Slice = timeSlice
@@ -188,6 +240,12 @@ class NSJF():
             self.Done_List.append(self.Running_Process) # append the complete process into done list
             self.Running_Process = None # set running process to none
 
+    def ReorderWaitingQueue(self):
+        temp = [] # create a temp list
+        for _ in range(self.Waiting_Queue.qsize()): temp.append(self.Waiting_Queue.get()) # copy the waiting queue to the temp list
+        temp.sort(key=lambda process: (process.CPU_Burst, process.Arrival_Time, process.ID)) # sort the temp process first by cpu burst second by arrival time third by PID
+        for process in temp: self.Waiting_Queue.put(process) # put the temp list back to the waiting queue
+
     def Print(self):
         print("==    NSJF==") # print label
         print(self.Gantt_Chart) # print gantt chart
@@ -197,10 +255,7 @@ class NSJF():
         for process in self.Process_List: process.CPU_Burst_Minus = process.CPU_Burst # add cpu burst minus
         while len(self.Done_List) < self.Process_Quantity: # while length of done list is less than the length of process list
             self.CheckProcess() # check the upcoming process
-            temp = [] # create a temp list
-            for _ in range(self.Waiting_Queue.qsize()): temp.append(self.Waiting_Queue.get()) # copy the waiting queue to the temp list
-            temp.sort(key=lambda process: (process.CPU_Burst, process.Arrival_Time, process.ID)) # sort the temp process first by cpu burst second by arrival time third by PID
-            for process in temp: self.Waiting_Queue.put(process) # put the temp list back to the waiting queue
+            self.ReorderWaitingQueue() # reorder the waiting queue
             self.Current_Time += 1 # current time + 1
             self.RunProcess() # run the current process or dispatch from waiting queue
         self.Print() # print the gantt chart
@@ -238,21 +293,25 @@ def main():
     elif method == 5: # PP (Preemptive Priority)
         PP_Process_List = copy.deepcopy(processList) # create a copy list
     elif method == 6: # ALL
-        FCFS_Process_List = copy.deepcopy(processList) # create a copy list
-        RR_Process_List = copy.deepcopy(processList) # create a copy list
-        NSJF_Process_List = copy.deepcopy(processList) # create a copy list
+        FCFS_Process_List = copy.deepcopy(processList) # create a FCFS process list
+        RR_Process_List = copy.deepcopy(processList) # create a RR process list
+        PSJF_Process_List = copy.deepcopy(processList) # create a PSJF process list
+        NSJF_Process_List = copy.deepcopy(processList) # create a NSJF process list
         FCFS_Simulate = FCFS(FCFS_Process_List, timeSlice) # create a new FCFS class
         FCFS_Simulate.Start() # start the FCFS class
         RR_Simulate = RR(RR_Process_List, timeSlice) # create a new FCFS class
         RR_Simulate.Start() # start the FCFS class
-        NSJF_Simulate = NSJF(NSJF_Process_List, timeSlice)
-        NSJF_Simulate.Start()
+        PSJF_Simulate = PSJF(PSJF_Process_List, timeSlice) # create a new PSJF class
+        PSJF_Simulate.Start() # start the PSJF class
+        NSJF_Simulate = NSJF(NSJF_Process_List, timeSlice) # create a new NSJF class
+        NSJF_Simulate.Start() # start the NJSF class
+
+        # print results
+        print("===========================================================\n")
         print("Waiting Time\nID      FCFS    RR      PSJF    NPSJF   Priority\n===========================================================")
-        for index in range(len(processList)):
-            print( index+1, "\t", FCFS_Simulate.Done_List[index].Waiting_Time, "\t", RR_Simulate.Done_List[index].Waiting_Time, "\t\t", NSJF_Simulate.Done_List[index].Waiting_Time )
+        for index in range(len(processList)): print( index+1, "\t", FCFS_Simulate.Done_List[index].Waiting_Time, "\t", RR_Simulate.Done_List[index].Waiting_Time, "\t", PSJF_Simulate.Done_List[index].Waiting_Time, "\t", NSJF_Simulate.Done_List[index].Waiting_Time )
         print("===========================================================\n\nTurnaround Time\nID      FCFS    RR      PSJF    NPSJF   Priority\n===========================================================")
-        for index in range(len(processList)):
-            print( index+1, "\t", FCFS_Simulate.Done_List[index].Turnaround_Time, "\t", RR_Simulate.Done_List[index].Turnaround_Time, "\t\t", NSJF_Simulate.Done_List[index].Turnaround_Time )
+        for index in range(len(processList)): print( index+1, "\t", FCFS_Simulate.Done_List[index].Turnaround_Time, "\t", RR_Simulate.Done_List[index].Turnaround_Time, "\t", PSJF_Simulate.Done_List[index].Turnaround_Time, "\t", NSJF_Simulate.Done_List[index].Turnaround_Time )
         print("===========================================================")
 
 if __name__ == "__main__":
