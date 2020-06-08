5aca572d1d32317e18607ea58ebbde75046fbe14
popshia <B123292179@gmail.com>
2020-06-08 15:58:03 +0800

    added output and organize everything, DONE!

--- CPU_Schedule.py
+++ CPU_Schedule.py
@@ -2,30 +2,7 @@
 # 10627130 資訊三甲 林冠良
 # DEAD LINE -> 6/12
 
-# 1: FCFS ( First Come First Serve )
-#   • 依arrival time先後次序處理
-#   • 若arrival time相同時，則依ProcessID由小至大依序處理。
-# 2: RR ( Round Robin )
-#   • 先依arrival time先後次序處理，時候未到的Process不能Run。
-#   • 若arrival time相同時，則依ProcessID由小至大依序處理。
-#   • Time out時，若有新來的Process，則讓新來的Process排在前面。
-#   • 某個Process Time slice未用完就結束時，必須讓下一個Process執行。
-# 3: PSJF ( Preemptive Shortest Job First )
-#   • 由剩餘CPU Burst最小的Process先處理。
-#   • 若剩餘的CPU Burst相同，讓沒有用過CPU的先使用，無法分別時則依arrival time小的先處理。
-#   • 若剩餘CPU Burst相同且arrival time相同，則依ProcessID由小至大依序處理。
-# 4: NSJF ( Non-preemptive Shortest Job First ) 
-#   • 由CPU Burst最小的Process先處理
-#   • 若CPU Burst最少的Process不只一個，則依 arrival time小的先處理
-#   • 若CPU Burst及arrival time相同，則依 ProcessID由小至大依序處理。
-# 5: PP ( Preemptive Priority )
-#   • Priority number小的代表Priority大
-#   • 依Priority由大致小依序處理
-#   • 若Priority相同，讓沒有用過CPU的先使用，無法分別時則依arrival time小的先處理
-#   • 若Priority及arrival time均相同，則依ProcessID由小至大依序處理。
-
 import copy
-from collections import deque
 
 class Process(): # process data structure
     def __init__(self, ID, CPU_Burst, arrivalTime, priority):
@@ -75,10 +52,6 @@ class FCFS(): # done
             self.Done_List.append(self.Running_Process) # append the complete process into done list
             self.Running_Process = None # set running process to none
 
-    def Print(self):
-        print("==    FCFS==") # print label
-        print(self.Gantt_Chart) # print gantt chart
-
     def Start(self):
         self.Process_List.sort(key=lambda process: (process.Arrival_Time, process.ID)) # sort the process first by arrival time second by process ID
         for process in self.Process_List: process.CPU_Burst_Minus = process.CPU_Burst # add cpu burst minus
@@ -86,7 +59,6 @@ class FCFS(): # done
             self.CheckProcess() # check the upcoming process
             self.Current_Time += 1 # current time + 1
             self.RunProcess() # run the current process or dispatch from waiting queue
-        self.Print() # print the gantt chart
         self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
 
 class RR(): # done
@@ -138,10 +110,6 @@ class RR(): # done
             self.Done_List.append(self.Running_Process) # append the complete process into done list
             self.Running_Process = None # set running process to none
 
-    def Print(self):
-        print("==      RR==") # print label
-        print(self.Gantt_Chart) # print gantt chart
-
     def Start(self):
         self.Process_List.sort(key=lambda process: (process.Arrival_Time, process.ID)) # sort the process first by arrival time second by process ID
         for process in self.Process_List: process.CPU_Burst_Minus = process.CPU_Burst # add cpu burst minus
@@ -149,7 +117,6 @@ class RR(): # done
             self.CheckProcess() # check the upcoming process
             self.Current_Time += 1 # current time + 1
             self.RunProcess() # run the current process or dispatch from waiting queue
-        self.Print() # print the gantt chart
         self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
 
 class PSJF(): # done
@@ -198,10 +165,6 @@ class PSJF(): # done
             self.Done_List.append(self.Running_Process) # append the complete process into done list
             self.Running_Process = None # set running process to none
 
-    def Print(self):
-        print("==    PSJF==") # print label
-        print(self.Gantt_Chart) # print gantt chart
-
     def Start(self):
         self.Process_List.sort(key=lambda process: (process.Arrival_Time, process.CPU_Burst, process.ID)) # sort the process first by cpu burst second by arrival time third by PID
         for process in self.Process_List: process.CPU_Burst_Minus = process.CPU_Burst # add cpu burst minus
@@ -210,7 +173,6 @@ class PSJF(): # done
             self.Waiting_Queue.sort(key=lambda process: (process.CPU_Burst_Minus, process.Has_Use_CPU, process.Arrival_Time, process.ID)) # reorder the waiting queue
             self.Current_Time += 1 # current time + 1
             self.RunProcess() # run the current process or dispatch from waiting queue
-        self.Print() # print the gantt chart
         self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
 
 class NPSJF(): # done
@@ -248,10 +210,6 @@ class NPSJF(): # done
             self.Done_List.append(self.Running_Process) # append the complete process into done list
             self.Running_Process = None # set running process to none
 
-    def Print(self):
-        print("==Non-PSJF==") # print label
-        print(self.Gantt_Chart) # print gantt chart
-
     def Start(self):
         self.Process_List.sort(key=lambda process: (process.Arrival_Time, process.CPU_Burst, process.ID)) # sort the process first by cpu burst second by arrival time third by PID
         for process in self.Process_List: process.CPU_Burst_Minus = process.CPU_Burst # add cpu burst minus
@@ -260,7 +218,6 @@ class NPSJF(): # done
             self.Waiting_Queue.sort(key=lambda process: (process.CPU_Burst, process.Arrival_Time, process.ID)) # reorder the waiting queue
             self.Current_Time += 1 # current time + 1
             self.RunProcess() # run the current process or dispatch from waiting queue
-        self.Print() # print the gantt chart
         self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
 
 class PP(): # done
@@ -309,10 +266,6 @@ class PP(): # done
             self.Done_List.append(self.Running_Process) # append the complete process into done list
             self.Running_Process = None # set running process to none
 
-    def Print(self):
-        print("==      PP==") # print label
-        print(self.Gantt_Chart) # print gantt chart
-
     def Start(self):
         self.Process_List.sort(key=lambda process: (process.Arrival_Time, process.Priority, process.ID)) # sort the process first by priority second by arrival time third by PID
         for process in self.Process_List: process.CPU_Burst_Minus = process.CPU_Burst # add cpu burst minus
@@ -321,10 +274,9 @@ class PP(): # done
             self.Waiting_Queue.sort(key=lambda process: (process.Priority, process.Has_Use_CPU, process.Arrival_Time, process.ID)) # reorder the waiting queue
             self.Current_Time += 1 # current time + 1
             self.RunProcess() # run the current process or dispatch from waiting queue
-        self.Print() # print the gantt chart
         self.Done_List.sort(key=lambda process: process.ID) # sort done list by PID
 
-def ReadProcess(input, processList):
+def ReadProcess(input, processList): # read process from imput
     input.readline() # read the labels
     tempArray = input.readline().split() # split every variables
     while tempArray != []: # while temp array is empty
@@ -332,17 +284,76 @@ def ReadProcess(input, processList):
         processList.append(singleProcess) # append this process to process list
         tempArray = input.readline().split() # read next process
 
-def PrintResult(doneList):
-    print("============\n")
-    print("Waiting Time\nID      Time\n============")
-    for index in range(len(doneList)): print( doneList[index].ID, "\t", doneList[index].Waiting_Time )
-    print("============\n\nTurnaround Time\nID      Time\n============")
-    for index in range(len(doneList)): print( doneList[index].ID, "\t", doneList[index].Turnaround_Time )
-    print("============")
+def PrintResult(simulate, inputFile): # print result for only one method
+    output = open(inputFile.name+"_output.txt", 'w') # open file
+    output.write("==Gantt_Ch==\n")
+    output.write(simulate.Gantt_Chart)
+    output.write("\n============\n\n") # print labels
+    output.write("Waiting Time\nID      Time\n============\n") # print labels
+    for index in range(len(simulate.Done_List)): # print waiting time
+        output.write(str(simulate.Done_List[index].ID))
+        output.write("\t\t")
+        output.write(str(simulate.Done_List[index].Waiting_Time))
+        output.write("\n")
+    output.write("============\n\nTurnaround Time\nID      Time\n============\n") # print labels
+    for index in range(len(simulate.Done_List)): # print waiting time
+        output.write(str(simulate.Done_List[index].ID))
+        output.write("\t\t")
+        output.write(str(simulate.Done_List[index].Turnaround_Time))
+        output.write("\n")
+    output.write("============") # print labels
+
+def PrintAllResult(inputFile, FCFS_Simulate, RR_Simulate, PSJF_Simulate, NPSJF_Simulate, PP_Simulate): # print every result
+    output = open(inputFile.name+"_output.txt", 'w') # open output file
+    output.write("==    FCFS==\n")
+    output.write(FCFS_Simulate.Gantt_Chart)
+    output.write("\n")
+    output.write("==      RR==\n")
+    output.write(RR_Simulate.Gantt_Chart)
+    output.write("\n")
+    output.write("==    PSJF==\n")
+    output.write(PSJF_Simulate.Gantt_Chart)
+    output.write("\n")
+    output.write("==Non-PSJF==\n")
+    output.write(NPSJF_Simulate.Gantt_Chart)
+    output.write("\n")
+    output.write("==      PP==\n")
+    output.write(PP_Simulate.Gantt_Chart)
+    output.write("\n")
+    output.write("===========================================================\n\n") # print labels
+    output.write("Waiting Time\nID      FCFS    RR      PSJF    NPSJF   Priority\n===========================================================\n") # print labels
+    for index in range(len(FCFS_Simulate.Done_List)): # print waiting time
+        output.write(str(FCFS_Simulate.Done_List[index].ID))
+        output.write("\t\t")
+        output.write(str(FCFS_Simulate.Done_List[index].Waiting_Time))
+        output.write("\t\t")
+        output.write(str(RR_Simulate.Done_List[index].Waiting_Time))
+        output.write("\t\t")
+        output.write(str(PSJF_Simulate.Done_List[index].Waiting_Time))
+        output.write("\t\t")
+        output.write(str(NPSJF_Simulate.Done_List[index].Waiting_Time))
+        output.write("\t\t")
+        output.write(str(PP_Simulate.Done_List[index].Waiting_Time))
+        output.write("\n")
+    output.write("===========================================================\n\nTurnaround Time\nID      FCFS    RR      PSJF    NPSJF   Priority\n===========================================================\n") # print labels
+    for index in range(len(FCFS_Simulate.Done_List)): # print turnaround time
+        output.write(str(FCFS_Simulate.Done_List[index].ID))
+        output.write("\t\t")
+        output.write(str(FCFS_Simulate.Done_List[index].Turnaround_Time))
+        output.write("\t\t")
+        output.write(str(RR_Simulate.Done_List[index].Turnaround_Time))
+        output.write("\t\t")
+        output.write(str(PSJF_Simulate.Done_List[index].Turnaround_Time))
+        output.write("\t\t")
+        output.write(str(NPSJF_Simulate.Done_List[index].Turnaround_Time))
+        output.write("\t\t")
+        output.write(str(PP_Simulate.Done_List[index].Turnaround_Time))
+        output.write("\n")
+    output.write("===========================================================") # print labels
 
 def main():
     #inputFile = open(input("Please enter the file name you want to simulate the scheduling...\n"), 'r') # open file
-    inputFile = open("input3.txt", 'r')
+    inputFile = open("input.txt", 'r')
     method, timeSlice = [int(token) for token in inputFile.readline().split()] # read method and time slice
     processList = [] # create an empty process list
     ReadProcess(inputFile, processList) # read the processes
@@ -351,27 +362,27 @@ def main():
         FCFS_Process_List = copy.deepcopy(processList) # create a FCFS process list
         FCFS_Simulate = FCFS(FCFS_Process_List) # create a new FCFS class
         FCFS_Simulate.Start() # start the FCFS class
-        PrintResult(FCFS_Simulate.Done_List) # print results
+        PrintResult(FCFS_Simulate, inputFile) # print results
     elif method == 2: # RR (Round Robin)
         RR_Process_List = copy.deepcopy(processList) # create a RR process list
         RR_Simulate = RR(RR_Process_List, timeSlice) # create a new RR class
         RR_Simulate.Start() # start the RR class
-        PrintResult(RR_Simulate.Done_List) # print results
+        PrintResult(RR_Simulate, inputFile) # print results
     elif method == 3: # PSJF (Preemptive Shortest Job First)
         PSJF_Process_List = copy.deepcopy(processList) # create a PSJF process list
         PSJF_Simulate = PSJF(PSJF_Process_List) # create a new PSJF class
         PSJF_Simulate.Start() # start the PSJF class
-        PrintResult(PSJF_Simulate.Done_List) # print results
+        PrintResult(PSJF_Simulate, inputFile) # print results
     elif method == 4: # NSJF (Non-preemptive Shortest Job First)
         NPSJF_Process_List = copy.deepcopy(processList) # create a NSJF process list
         NPSJF_Simulate = NPSJF(NPSJF_Process_List) # create a new NSJF class
         NPSJF_Simulate.Start() # start the NJSF class
-        PrintResult(NPSJF_Simulate.Done_List) # print results
+        PrintResult(NPSJF_Simulate, inputFile) # print results
     elif method == 5: # PP (Preemptive Priority)
         PP_Process_List = copy.deepcopy(processList) # create a PP process list
         PP_Simulate = PP(PP_Process_List)     # create a new PP class
         PP_Simulate.Start() # start the PP class
-        PrintResult(PP_Simulate.Done_List) # print results
+        PrintResult(PP_Simulate, inputFile) # print results
     elif method == 6: # ALL
         FCFS_Process_List  = copy.deepcopy(processList) # create a FCFS process list
         RR_Process_List    = copy.deepcopy(processList) # create a RR process list
@@ -388,13 +399,7 @@ def main():
         PSJF_Simulate.Start()  # start the PSJF class
         NPSJF_Simulate.Start() # start the NJSF class
         PP_Simulate.Start()    # start the PP class
-        # print results
-        print("===========================================================\n")
-        print("Waiting Time\nID      FCFS    RR      PSJF    NPSJF   Priority\n===========================================================")
-        for index in range(len(processList)): print( FCFS_Simulate.Done_List[index].ID, "\t", FCFS_Simulate.Done_List[index].Waiting_Time, "\t", RR_Simulate.Done_List[index].Waiting_Time, "\t", PSJF_Simulate.Done_List[index].Waiting_Time, "\t", NPSJF_Simulate.Done_List[index].Waiting_Time, "\t", PP_Simulate.Done_List[index].Waiting_Time )
-        print("===========================================================\n\nTurnaround Time\nID      FCFS    RR      PSJF    NPSJF   Priority\n===========================================================")
-        for index in range(len(processList)): print( FCFS_Simulate.Done_List[index].ID, "\t", FCFS_Simulate.Done_List[index].Turnaround_Time, "\t", RR_Simulate.Done_List[index].Turnaround_Time, "\t", PSJF_Simulate.Done_List[index].Turnaround_Time, "\t", NPSJF_Simulate.Done_List[index].Turnaround_Time, "\t", PP_Simulate.Done_List[index].Turnaround_Time )
-        print("===========================================================")
+        PrintAllResult(inputFile, FCFS_Simulate, RR_Simulate, PSJF_Simulate, NPSJF_Simulate, PP_Simulate) # print all results
 
 if __name__ == "__main__":
     main()
